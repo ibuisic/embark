63b041eac89220e6c555d3843c91f91a7645fa2e
Ivan Buisic <buisic.ivan@gmail.com>
2019-11-14 14:52:21 +0100

    Added loaders

---
+++ build/app.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["app"],{
+
+/***/ "./assets/js/app.js":
+/*!**************************!*\
+  !*** ./assets/js/app.js ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+
+
+/***/ })
+
+},[["./assets/js/app.js","runtime"]]]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VSb290IjoiIn0=
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const Encore = require('@symfony/webpack-encore');
 
 Encore
     // directory where compiled assets will be stored
-    .setOutputPath('./build/')
+    .setOutputPath('build/')
     // public path used by the web server to access the output path
-    .setPublicPath('./build')
+    .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
