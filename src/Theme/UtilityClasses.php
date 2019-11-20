79fd11e66b276ff7bed2ae68e4f023d712f4ca34
Ivan Buisic <buisic.ivan@gmail.com>
2019-11-20 13:11:14 +0100

    A bucko problem

--- src/Theme/UtilityClasses.php
+++ src/Theme/UtilityClasses.php
@@ -27,10 +27,11 @@ class UtilityClasses {
     $config = array_merge($defaults, $options);
 
     if (theme_get_setting($theme_setting)  !== '') {
-      $variables[$theme_setting] = preg_replace("/[^a-z0-9_\s-]/", "", strtolower(theme_get_setting($theme_setting)));
+      $classes = preg_replace("/[^a-z0-9_\s-]/", "", strtolower(theme_get_setting($theme_setting)));
+      $variables[$theme_setting] = $classes;
     }
     if ($config['addToAttributes']) {
-      $variables['attributes']['class'][] = $variables[$theme_setting];
+      $variables['attributes']['class'][] = $classes;
     }
 
     return $variables;
